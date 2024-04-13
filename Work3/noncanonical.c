aa30ada04c121dd02834131b12fcdb86ccee3c0d
Valentino <valentinocarneirogoncalves@gmail.com>
2024-04-13 17:08:48 +0100

    Added functions for better construction blocks

--- Work3/noncanonical.c
+++ Work3/noncanonical.c
@@ -13,6 +13,42 @@
 
 volatile int STOP=FALSE;
 
+int receive_handshake(int fd) {
+    char buf[6];
+
+    while(STOP == FALSE){
+        int res = read(fd,buf,5);   /* returns after 5 chars have been input */
+        buf[res]=0;               /* so we can printf... */
+    }
+
+    fprintf("[INFO] Received SET packet\n");
+
+    char correct_answer [5] = {0x5c, 0x03, 0x08, "", 0x5c};
+    correct_answer[3]  = buf[1]^buf[2];
+
+	if (strncmp(correct_answer, buf, 5) == 0) {
+	    
+        fprintf("[SUC] SET packet not corrupted\n");
+
+	    char UA [5] = {0x5c, 0x03, 0x06, "", 0x5c};
+
+	    UA[3] = UA[1]^UA[2];
+
+	    res = write(fd, UA, 5);
+		
+        printf("[INFO] Sent UA packet\n");
+        printf("[INFO] Connection established\n");
+
+        return 0;
+	}  	    
+
+    else {
+        fprintf("[ERR] SET packet corrupted\n");
+        return -1;
+    }
+
+}
+
 int main(int argc, char** argv)
 {
     int fd,c, res;
@@ -25,7 +61,6 @@ int main(int argc, char** argv)
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
-
 
     /*
     Open serial port device for reading and writing and not as controlling tty
@@ -67,42 +102,8 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-    while (STOP==FALSE) {       /* loop for input */
-        res = read(fd,buf,5);   /* returns after 5 chars have been input */
-        buf[res]=0;               /* so we can printf... */
-	
-	//Check if it arrived sucessfully
-
-	printf("0x%x 0x%x 0x%x 0x%x 0x%x\n", buf[0], buf[1], buf[2], buf[3], buf[4]);
-
-        char correct_answer [5] = {0x5c, 0x03, 0x08, "", 0x5c};
-
-	correct_answer[3]  = buf[1]^buf[2];
-
-	if (strncmp(correct_answer, buf, 5) == 0) {
-	    printf("Sucess\n");
-	    //Sending back the UA packet.
-
-	    char UA [5] = {0x5c, 0x03, 0x06, "", 0x5c};
-
-	    UA[3] = UA[1]^UA[2];
-
-	    res = write(fd, UA, 5);
-		
-            printf("Bye\n");
-	}  	        
-
-	
-
-        //printf(":%s:%d\n", buf, res);
-        //if (buf[0]=='z') STOP=TRUE;
-    }
-
-
-
-    /*
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião
-    */
+    receive_handshake(fd);
+}
 
     sleep(1);
     tcsetattr(fd,TCSANOW,&oldtio);
--- Work3/writenoncanonical.c
+++ Work3/writenoncanonical.c
@@ -16,6 +16,80 @@
 
 volatile int STOP=FALSE;
 
+int handshake(int fd) {
+    fprintf("[INFO] Initializing Handshake \n");
+
+	const char flag = 0x5c;
+	const char address = 0x03;
+	const char control = 0x08;
+	const char bcc1 = address^control;
+	
+    char buffer[5]= {flag, address, control, bcc1, flag};
+
+    res = write(fd,buffer,5);
+
+    if (res == 5) {
+        fprintf("[INFO] Sent SET packet\n");
+    } else {
+        fprintf("[ERR] Error sending SET packet\n");
+        return -1;
+    }
+
+    char buf[5];
+
+    while (STOP==FALSE) {       /* loop for input */
+        int res = read(fd,buf,5);   /* returns after 5 chars have been input */
+        buf[res]=0;               /* so we can printf... */
+
+        char received_correct = 1;
+        if (memcmp(buf, &flag, 1)==0){
+			//printf("Flag correct\n");
+		}else{
+			received_correct=0;
+		}
+		
+		if (memcmp(buf+1, &address, 1)==0){
+			//printf("Address correct\n");
+		}else{
+			received_correct=0;
+		}
+		
+		if (memcmp(buf+2, &control, 1)==0){
+			//printf("Control is SET\n");
+		}else{
+			received_correct=0;
+		}
+		
+		char xor_shouldbe = *(buf+1) ^ *(buf+2);
+		if (memcmp(buf+3, &xor_shouldbe, 1)==0){
+			//printf("BCC is correct\n");
+		}else{
+			received_correct=0;
+		}
+		
+		if (memcmp(buf+4, &flag, 1)==0){
+			//printf("END flag is correct\n");
+		}else{
+			received_correct=0;
+		}
+		
+		if (received_correct){
+			fprintf("[INFO] Received UA packet\n");
+			
+		}
+        else{
+            fprintf("[ERR] Corrupted UA packet\n");
+            return -1;
+        }
+		
+    }
+
+    fprintf("[INFO] Connection established\n");
+    return 0;
+
+
+}
+
 int main(int argc, char** argv)
 {
     int fd,c, res;
@@ -73,78 +147,7 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-
-
-    
-
-	const char flag = 0x5c;
-	const char address = 0x03;
-	const char control = 0x08;
-	const char bcc1 = address^control;
-	
-    char buffer[5]= {flag, address, control, bcc1, flag};
-
-    res = write(fd,buffer,5);
-    printf("%d bytes written\n", res);
-
-
-	char respond_buff[32];
-	
-	
-	
-    while (STOP==FALSE) {       /* loop for input */
-        res = read(fd,buf,5);   /* returns after 5 chars have been input */
-        buf[res]=0;               /* so we can printf... */
-        printf(":%s:%d\n", buf, res);
-        char received_correct = 1;
-        if (memcmp(buf, &flag, 1)==0){
-			printf("Flag correct\n");
-		}else{
-			received_correct=0;
-		}
-		
-		if (memcmp(buf+1, &address, 1)==0){
-			printf("Address correct\n");
-		}else{
-			received_correct=0;
-		}
-		
-		if (memcmp(buf+2, &control, 1)==0){
-			printf("Control is SET\n");
-		}else{
-			received_correct=0;
-		}
-		
-		char xor_shouldbe = *(buf+1) ^ *(buf+2);
-		if (memcmp(buf+3, &xor_shouldbe, 1)==0){
-			printf("BCC is correct\n");
-		}else{
-			received_correct=0;
-		}
-		
-		if (memcmp(buf+4, &flag, 1)==0){
-			printf("END flag is correct\n");
-		}else{
-			received_correct=0;
-		}
-		
-		if (received_correct){
-			printf("Everything good\n");
-			// if everything is received correctly we respond
-			respond_buff[0]=flag;
-			respond_buff[1]=address;
-			respond_buff[2]=control;
-			respond_buff[3]=control ^ address;
-			respond_buff[4]=flag;
-		}
-		
-		STOP=TRUE;
-    }
-
-    /*
-    O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar
-    o indicado no guião
-    */
+    handshake(fd);
 
     sleep(1);
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
