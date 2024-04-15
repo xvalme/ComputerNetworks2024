1f7ef22421f6c834506c3dac5de20c4fa7406c05
xval <valentinocarneirogoncalves@gmail.com>
2024-04-15 16:45:09 +0100

    updates  to information frames

--- Work3/noncanonical.c
+++ Work3/noncanonical.c
@@ -13,7 +13,9 @@
 
 volatile int STOP=FALSE;
 
-int receive_handshake(int fd) {
+#define DATA_BUFFER_SIZE 1024;
+
+int handshake(int fd) {
     char buf[6];
     int temporary_size = 6;
     char temporary[temporary_size];
@@ -109,8 +111,16 @@ int receive_handshake(int fd) {
                         UA[3] = UA[1]^UA[2];
 
                         int res = write(fd, UA, 5);
+                        int count = 2; 
 
-                        printf("%d\n", res);
+                        while (res != 5) {
+                            res = write(fd, UA, 5);
+                            count--;
+                            if (!count) {
+                                return -1;
+                            }
+                        }
+                        printf("[INFO] Sent UA\n");
 
                         return 1;
                     }
@@ -132,18 +142,167 @@ int receive_handshake(int fd) {
     }
 }
 
+int receive_data(int fd){
+
+    int res;
+    char buf[DATA_BUFFER_SIZE];
+    char temporary[DATA_BUFFER_SIZE];
+    char data [DATA_BUFFER_SIZE];
+    int STATE = 0;
+
+    char FLAG = 0x5c;
+    char Add_S = 0x03;
+    char Add_R = 0x01;
+    
+    char Ctrl_up = 0b11000000;
+    char Ctrl_down = 0b10000000;
+
+    char current_ctrl_int = 1;
+    char current_ctrl = Ctrl_down;
+    
+    int number_bytes_received = 0;
+    char moving_xor = '';
+
+
+    while(TRUE){
+
+        if (STOP == FALSE) {
+
+            if (current_ctrl_int == 1) {
+                current_ctrl = Ctrl_down;
+            } else {
+                current_ctrl = Ctrl_up;
+            }
+
+            res = read(fd,buf,1);   
+            buf[res]=0;       
+
+            if (res) {
+                STOP = TRUE;
+                continue;
+            } 
+        }
+
+        if (STOP == TRUE){
+            //Again we use a switch/case scenario
+            //States > 0 F1 A C B1 D B2 F2 Z
+            //With num:0 1  2 3 4  5    7  
+
+            switch (STATE) {
+                case 0:
+                    {
+                    if (buf[0] == FLAG){
+                        STATE = 1;
+                        temporary[0] = buf[0];
+                    }
+                    break;
+                    }
+
+                case 1:
+                    if (buf[0] == Add_S) {
+                        STATE = 2;
+                        temporary[1] = buf[0];
+                    }
+                    else if (buf[0] == FLAG) {
+                        STATE = 1;
+                    }
+                    else {
+                        STATE = 0;
+                        memset (temporary, 0, DATA_BUFFER_SIZE );
+                    }
+                    break;
+
+                case 2:
+                    if (buf[0] == current_ctrl) {
+                        STATE = 3;
+                        temporary[2] = buf[0];
+                        current_ctrl_int = !current_ctrl_int;
+                    }
+                    else if (buf[0] == Add_S) {
+                        STATE = 2;
+                    }
+                    else {
+                        STATE = 0;
+                        memset (temporary, 0, DATA_BUFFER_SIZE );
+                    }
+                    break;
+
+                case 3:
+                    if (buf[0] == (temporary[1]^temporary[2])) {
+                        STATE = 4;
+                        temporary[3] = buf[0];
+                    }
+                    else if (buf[0] == current_ctrl) {
+                        STATE = 3;
+                    }
+                    else {
+                        STATE = 0;
+                        memset (temporary, 0, DATA_BUFFER_SIZE );
+                    }
+                    break;
+                
+                case 4:
+                    //We now receive the data.
+
+                    if (buf[0] == FLAG) {
+                        STATE = 0;
+                        memset (temporary, 0, DATA_BUFFER_SIZE );
+                    }
+                    else if (buf[0] == moving_xor) {
+                        STATE = 5;
+                        temporary[4] = buf[0];
+                    }
+                    else {
+                        data[number_bytes_received] = buf[0];
+                        number_bytes_received++;
+                        moving_xor = data[number_bytes_received]^moving_xor;
+                    }
+                    break;
+
+                case 5:
+                    if (buf[0] == moving_xor) {
+                        STATE = 6:
+                        temporary[5] = buf[0];
+                    }
+                    else {
+                        //Fail to receive data
+                        return -1;
+                    }
+
+                    break;
+
+                case 6:
+                    if (buf[0] == FLAG) {
+                        //Received data correctly 
+                        return 1;
+                    }
+                    else {
+                        //Fail to receive data
+                        return -1;
+                    }
+
+                    break;
+                    
+            STOP = FALSE;
+            
+        }
+
+}
+
 int main(int argc, char** argv)
 {
     int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
 
+    /*Comment to use virtual ports
     if ( (argc < 2) ||
          ((strcmp("/dev/ttyS0", argv[1])!=0) &&
           (strcmp("/dev/ttyS1", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
+    */
 
     /*
     Open serial port device for reading and writing and not as controlling tty
@@ -185,7 +344,8 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-    receive_handshake(fd);
+    handshake(fd);
+    receive_data(fd);
 
     sleep(1);
     tcsetattr(fd,TCSANOW,&oldtio);
--- Work3/writenoncanonical.c
+++ Work3/writenoncanonical.c
@@ -127,12 +127,14 @@ int main(int argc, char** argv)
     char buf[255];
     int i, sum = 0, speed = 0;
 
+    /*Comment to use virtual ports
     if ( (argc < 2) ||
          ((strcmp("/dev/ttyS0", argv[1])!=0) &&
           (strcmp("/dev/ttyS1", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
+    */
 
 
     /*
