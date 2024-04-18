9b43bd418ea17f769d8a061d5be3431a5246790f
RomanFilip <xroman16@vutbr.cz>
2024-04-18 22:17:35 +0100

    send and receive data

--- Work3/writenoncanonical.c
+++ Work3/writenoncanonical.c
@@ -16,13 +16,17 @@
 
 volatile int STOP=FALSE;
 
+#define DATA_BUFFER_SIZE 1024
+
 typedef enum {
     Start_State,
     Flag_RCV_State,
     A_RCV_State,
     C_RCV_State,
     BCC_OK_State,
-    Stop_State
+    Stop_State,
+    DATA_RCV_State,
+    BCC2_OK_State
 } StateMachine;
 
 int handshake(int fd) {
@@ -104,45 +108,144 @@ int handshake(int fd) {
                 state = Start_State;
             }
             break;
-        }
-
-		
+        }	
     }
     return 0;
 }
 
-int send_dummy_data(int fd){
+int receive_data(int fd) {
+    const char flag = 0x5c;
+	const char address = 0x03;
+	const char control = 0x08;
+	const char control_ua = 0x06;
 
-    char test_packet[] = {0x5c, 0x01, 0 , 0 , 'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!', 'X',0, 0x5c};
+	const char bcc1 = address^control;  
+	const char bcc1_ua = address^control_ua;
 
-    test_packet[2] = 0xc0;
-    test_packet[3] = test_packet[1]^test_packet[2];
+    char buf[1];
+    StateMachine state = Start_State;
+    int consecutive_flags = 0;
+    char received_data[DATA_BUFFER_SIZE];
+    int data_index = 0;
 
-    char bcc2 = ' ';
-    for (int i = 4; i < 18; i++) {
-        printf("%x|", bcc2);
-        bcc2 ^= test_packet[i];
-    }
+	
+    while (STOP==FALSE || state != Stop_State) {       /* loop for input */
+        int res = read(fd,buf,1);   /* returns after 5 chars have been input */
+        if (res < 0) {
+            fprintf(stderr, "[ERR] Error reading from serial port\n");
+            return -1;
+        }
+        // char received_correct = 1;
+        if (buf[0] == flag) {
+            consecutive_flags++;
+        } else {
+            consecutive_flags = 0;
+        }
 
-    int pos = sizeof(test_packet)-2 ;
-
-    test_packet[pos] = bcc2;
-
-    for (int i = 0; i < sizeof(test_packet); i++) {
-        fprintf(stderr, "%x ", test_packet[i]);
-    }
-
-    int res = write(fd,test_packet, sizeof(test_packet));
+        if (consecutive_flags == 2) {
+            // two consecutive flags, end of data
+            fprintf(stderr, "[INFO] Received end of data\n");
+            break;
+        }
     
-    if (res == sizeof(test_packet)) {
-        fprintf(stderr, "[INFO] Sent dummy data packet\n");
+        switch (state) {
+            case Start_State:
+                if (buf[0] == flag) {
+                    state = Flag_RCV_State;
+                }
+                break;
+
+            case Flag_RCV_State:
+                if (buf[0] == address) {
+                    state = A_RCV_State;
+                } else {
+                    state = Start_State;
+                }
+                break;
+
+            case A_RCV_State:
+                if (buf[0] == control_ua) {
+                    state = C_RCV_State;
+                } else {
+                    state = Start_State;
+                }
+                break;
+
+            case C_RCV_State:
+                if (buf[0] == bcc1_ua) {
+                    state = BCC_OK_State;
+                } else {
+                    state = Start_State;
+                }
+                break;
+
+            case BCC_OK_State:
+                if (buf[0] == flag) {
+                    state = DATA_RCV_State;
+                } else {
+                    state = Start_State;
+                }
+                break;
+
+            case DATA_RCV_State:
+                // Save received data
+                if (buf[0] != flag) {
+                    if (data_index < DATA_BUFFER_SIZE) {
+                        received_data[data_index++] = buf[0];
+                    } else {
+                        fprintf(stderr, "[ERR] Received data too large\n");
+                        return -1;
+                    }
+                } else {
+                    state = BCC2_OK_State;
+                }
+                break;
+
+            case BCC2_OK_State:
+                if (buf[0] == flag) {
+                    state = Stop_State;
+                    // Print received data
+                    printf("[INFO] Received data: %s\n", received_data);
+                    return 0;
+                } else {
+                    state = Start_State;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+    return 0;
+}
+
+int send_data(int fd, const char* data, int data_length) {
+    const char flag = 0x5c;
+    const char address = 0x03;
+    const char control = 0x08;
+
+    char buffer[data_length + 5]; // Data length + 1 address byte + 1 control byte + 2 flag bytes
+    buffer[0] = flag; // Start flag
+    buffer[1] = address;
+    buffer[2] = control;
+    for (int i = 0; i < data_length; ++i) {
+        buffer[i + 3] = data[i];
+    }
+    char bcc = address ^ control;
+    for (int i = 0; i < data_length; ++i) {
+        bcc ^= data[i];
+    }
+    buffer[data_length + 3] = bcc;
+    buffer[data_length + 4] = flag; // End flag
+
+    int res = write(fd, buffer, data_length + 5);
+    if (res == data_length + 5) {
+        fprintf(stderr, "[INFO] Sent data successfully\n");
+        return 0;
     } else {
-        fprintf(stderr, "[ERR] Error sending dummy data packet\n");
+        fprintf(stderr, "[ERR] Error sending data\n");
         return -1;
     }
-
-    return 0;
-
 }
 
 int main(int argc, char** argv)
@@ -152,11 +255,11 @@ int main(int argc, char** argv)
     char buf[255];
     int i, sum = 0, speed = 0;
 
-    /*if ( (argc < 2) ||
+    if ( (argc < 2) ||
          ((strcmp("/dev/ttyS10", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS10\n");
         exit(1);
-    }*/
+    }
 
 
     /*
@@ -207,7 +310,16 @@ int main(int argc, char** argv)
         exit(-1);
     }
 
-    send_dummy_data(fd);
+    const char data[] = "Hello, world!";
+    if (send_data(fd, data, strlen(data)) != 0) {
+        fprintf(stderr, "[ERR] Error in sending data\n");
+        exit(-1);
+    }
+
+    if (receive_data(fd) != 0) {
+        fprintf(stderr, "[ERR] Error in receiving data\n");
+        exit(-1);
+    }
 
     sleep(1);
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
